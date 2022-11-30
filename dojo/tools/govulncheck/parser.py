69eca7217de8edc72ae94d54fa9fe312d6448f62
Denis Timoshenko <d.timoshenko@it.ecommpay.com>
2022-11-30 18:40:27 +0300

    update parser govulncheck

--- dojo/tools/govulncheck/parser.py
+++ dojo/tools/govulncheck/parser.py
@@ -1,9 +1,11 @@
 import json
-
+from itertools import groupby
 from dojo.models import Finding
 
+SEVERITY = 'Info'
 
-class govulncheckParser:
+
+class GovulncheckParser:
 
     def get_scan_types(self):
         return ["Govulncheck Scanner"]
@@ -14,72 +16,50 @@ class govulncheckParser:
     def get_description_for_scan_types(self, scan_type):
         return "Import Govulncheck Scanner findings in JSON format."
 
+    def get_location(self, data, node):
+        call = data['Calls']['Functions'][str(node)]
+        location = f"{call['CallSites'][0]['Pos']['Filename']}:{str(call['CallSites'][0]['Pos']['Line'])}:" \
+                   f"{str(call['CallSites'][0]['Pos']['Column'])}"
+        return location
+
     def get_findings(self, scan_file, test):
+
+        findings = []
 
         # get data from report.json
         scan_data = scan_file.read()
-        # remove into from developer
-        pos_json = scan_data.find(b'{')
-        scan_data = scan_data[pos_json:]
+        # remove intro from developer
+        scan_data = scan_data[scan_data.find(b'{'):]
         data = json.loads(scan_data)
 
-        # init lists
-        findings = []
-        ignore = []
+        list_vulns = data['Vulns']
 
-        # cycle for every object from block Vulns
-        for vuln in data['Vulns']:
-            if vuln['OSV']['aliases'] in ignore:
-                continue
+        for cve, elems in groupby(list_vulns, key=lambda vuln: vuln['OSV']['aliases'][0]):
+            collected = False
+            vuln_methods = set()
+            impact = set()
+            title = str()
+            description = str()
+            references = str()
+            url = str()
+            for elem in elems:
+                if not collected:
+                    title = elem['OSV']['id']
+                    description = elem['OSV']['details']
+                    references = elem['OSV']['references'][0]['url']
+                    url = elem['OSV']['affected'][0]['database_specific']['url']
+                    collected = True
+                impact.add(self.get_location(data, elem['CallSink']))
+                vuln_methods.update(elem['OSV']['affected'][0]['ecosystem_specific']['imports'][0]['symbols'])
 
-            # get info from field
-            refs = []
-            vuln_files = []
-            node = vuln['CallSink']
-            call = data['Calls']['Functions'][str(node)]
-            location = call['CallSites'][0]['Pos']['Filename'] + ':' + str(call['CallSites'][0]['Pos']['Line']) + ':' \
-                       + str(call['CallSites'][0]['Pos']['Column'])
-            descrip_vuln = vuln['OSV']
-            title = descrip_vuln['id']
-            date = descrip_vuln['published']
-            date = date[0:10]
-            cve = descrip_vuln['aliases'][0]
-            ignore.append(cve)
-            description = descrip_vuln['details']
-            url = descrip_vuln['affected'][0]['database_specific']['url']
-            impact = descrip_vuln['affected'][0]['ecosystem_specific']['imports'][0]['symbols']
-
-            vuln_files.append(location)
-            for i in descrip_vuln['references']:
-                refs.append(i['url'])
-
-            # find vuln with same CVE and collecting additional information
-            for similar_vuln in data['Vulns']:
-                if similar_vuln['OSV']['aliases'][0] == cve:
-                    similar_node = similar_vuln['CallSink']
-                    call = data['Calls']['Functions'][str(similar_node)]
-                    new_location = call['CallSites'][0]['Pos']['Filename'] + ':' + str(
-                        call['CallSites'][0]['Pos']['Line']) + ':' \
-                                   + str(call['CallSites'][0]['Pos']['Column'])
-                    vuln_files.append(new_location)
-
-            # prepare info
-            references = '\n'.join(str(i) for i in refs)
-            impact = 'Vulnerable method: ' + ", ".join(str(i) for i in impact)
-            impact = impact + "\nVulnerable endpoints: \n" + '\n'.join(str(i) for i in vuln_files)
-            severity = 'Info'
-
-            # create Finding
-            finding = Finding(
+            findings.append(Finding(
                 title=title,
                 cve=cve,
                 references=references,
                 description=description,
                 url=url,
-                impact=impact,
-                severity=severity,
-            )
-            # Finding put in list
-            findings.append(finding)
-
+                impact='\n'.join(impact),
+                severity=SEVERITY,
+                component_name='; '.join(vuln_methods)
+            ))
         return findings
